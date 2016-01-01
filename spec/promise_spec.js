3d889e141de525bccb9821ed3de4c1d012adb3a3
Your Name <you@example.com>
2016-01-01 23:07:21 +0800

    move to one file with node to run

--- spec/promise_spec.js
+++ spec/promise_spec.js
@@ -2,9 +2,9 @@ var Defer = require('../src/main')
 
 describe("Defer", function(){
 	it("should support then", function(){
-		var defer = new Defer();
-		defer.then(function() {console.log("get result from server")});
-		console.log("asdf");
-		expect(true).toBe(true);
+		// var defer = new Defer();
+		// defer.then(function() {console.log("get result from server")});
+		// console.log("asdf");
+		// expect(true).toBe(true);
 	})
 })
--- src/main.js
+++ src/main.js
@@ -3,26 +3,37 @@ var Promise = function(){
 }
 
 Promise.prototype.then = function(success){
+	console.log("add success");
 	this.successesCallback.push(success);
 }
 
 var Defer = function(){
-	var promise = new Promise();
-	console.log(promise);
-	
-	var remoteCall = function(callBack){
-		for(var i = 0; i < 1000000000; i++){
-		}
-		console.log("after remote call");
-		console.log(callbck);
-		callBack();
-	}
-
-	remoteCall(promise.successesCallback[0])
-
-
-
-	return promise;
+	this.promise = new Promise();
 }
 
-module.exports = Defer;
+Defer.prototype.resolve = function(){
+	console.log("defer resolve is calling");
+	console.log("2promise of defer:" + this.promise)
+	this.promise.successesCallback[0]();
+}
+
+var remoteCall = function(callBack){
+	for(var i = 0; i < 1000000000; i++){
+	}
+	callBack();
+}
+
+var callRemote = function(){
+	var defer = new Defer();
+	console.log("promise of defer:" + defer.promise)
+	console.log("set timer for remote call");
+	setTimeout(function(){remoteCall(defer.resolve)}, 0);
+	console.log("remote call is triggered");
+
+	return defer.promise;
+}
+
+callRemote().then(function(){console.log("Hello, server call done")});
+
+
+// module.exports = Defer;
